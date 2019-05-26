507d4de453e5b58e95f65d9a43075c1920ab17c9
Liudmila Azarenko <mila.azarenko@gmail.com>
2019-05-26 20:21:40 +0300

    DistinctWords: added a few assertions

--- src/main/java/tasks/Main.java
+++ src/main/java/tasks/Main.java
@@ -8,10 +8,10 @@ import tasks.reader.AbstractFileReader;
 import tasks.reader.CustomFileReader;
 import tasks.words.WordProcessor;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
+
+import static org.junit.gen5.api.Assertions.assertEquals;
+import static org.junit.gen5.api.Assertions.assertNotEquals;
 
 /**
  * tasks.Main class of this application
@@ -32,26 +32,33 @@ public class Main {
         final List<String> fileData = reader.getFileData();
         final WordProcessor processor = new WordProcessor(fileData);
         processor.processData();
+        final Set<String> distinctWords = processor.getDistinctWords();
 
-        LOG.info("Found distinct tasks.words: {}", processor.getDistinctWords());
+        LOG.info("Found distinct tasks.words: {}", distinctWords);
+        assertEquals(distinctWords.size(), 2, "Distinct words number should be 2");
 
         // Task #2
         LOG.info("Performing task #2");
         final Integer[] numbers = new Integer[] {10, 22, 17, 22, 44};
+        final int length = numbers.length;
 
         LOG.info("Given array of {} numbers:", numbers.length);
-        for(int i = 0; i < numbers.length; i++) {
+        for(int i = 0; i < length; i++) {
             LOG.info("{}", numbers[i]);
         }
 
-        LOG.info("It will be transformed to collection.");
+        LOG.info("It will be transformed to collections.");
         final ArrayToCollectionTransformer<Integer> collectionTransformer = new ArrayToCollectionTransformer<Integer>();
         final Collection numbersList =
                 collectionTransformer.putObjectsFromArrayToCollection(numbers, new ArrayList<>());
         final Collection numbersSet =
                 collectionTransformer.putObjectsFromArrayToCollection(numbers, new HashSet<>());
+
         LOG.info("Obtained list: {}", numbersList);
+        assertEquals(numbersList.size(), length, "Array and ArraList sizes are not equal");
         LOG.info("Obtained set: {}", numbersSet);
+        assertNotEquals(numbersSet.size(), length,
+                String.format("Array has duplicate. Resulting set's size should be less than %d", length));
 
         // Task #3
         LOG.info("Performing task #3");
